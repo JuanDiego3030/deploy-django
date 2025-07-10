1c02a5859aa67889b48ace84a08eaecd3acad2d1
JuanDiego3030 <juandiegoaranaperez@gmail.com>
2025-07-10 12:39:49 -0400

    fix permisos

--- django_install.sh
+++ django_install.sh
@@ -47,53 +47,58 @@ read -p 'Indique la el nombre de la carpeta del proyecto (django-father): ' proj
 read -p 'Indique el nombre de la app principal de Django (father): ' djapp
 
 echo "==13== Instalamos las dependencias === "
-pip install -q -r /home/django/$project/requirements.txt
+sudo -u django /home/django/.venv/bin/pip install -q -r /home/django/$project/requirements.txt
 
 echo "==14== Instalamos Gunicorn === "
-pip install -q gunicorn
+sudo -u django /home/django/.venv/bin/pip install -q gunicorn
 
-touch /home/django/.venv/bin/gunicorn_start
-chmod u+x /home/django/.venv/bin/gunicorn_start
-echo '#!/bin/bash' >> /home/django/.venv/bin/gunicorn_start
-echo '' >> /home/django/.venv/bin/gunicorn_start
-echo 'NAME="django_app"' >> /home/django/.venv/bin/gunicorn_start
-echo 'DIR=/home/django/'$project >> /home/django/.venv/bin/gunicorn_start
-echo 'USER=django' >> /home/django/.venv/bin/gunicorn_start
-echo 'GROUP=django' >> /home/django/.venv/bin/gunicorn_start
-echo 'WORKERS=3' >> /home/django/.venv/bin/gunicorn_start
-echo 'BIND=unix:/home/django/gunicorn.sock' >> /home/django/.venv/bin/gunicorn_start
-echo 'DJANGO_SETTINGS_MODULE='$djapp'.settings' >> /home/django/.venv/bin/gunicorn_start
-echo 'DJANGO_WSGI_MODULE='$djapp'.wsgi' >> /home/django/.venv/bin/gunicorn_start
-echo 'LOG_LEVEL=error' >> /home/django/.venv/bin/gunicorn_start
-echo '' >> /home/django/.venv/bin/gunicorn_start
-echo 'source /home/django/.venv/bin/activate' >> /home/django/.venv/bin/gunicorn_start
-echo '' >> /home/django/.venv/bin/gunicorn_start
-echo 'export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE' >> /home/django/.venv/bin/gunicorn_start
-echo 'export PYTHONPATH=$DIR:$PYTHONPATH' >> /home/django/.venv/bin/gunicorn_start
-echo '' >> /home/django/.venv/bin/gunicorn_start
-echo 'exec /home/django/.venv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \' >> /home/django/.venv/bin/gunicorn_start
-echo '  --name $NAME \' >> /home/django/.venv/bin/gunicorn_start
-echo '  --workers $WORKERS \' >> /home/django/.venv/bin/gunicorn_start
-echo '  --user=$USER \' >> /home/django/.venv/bin/gunicorn_start
-echo '  --group=$GROUP \' >> /home/django/.venv/bin/gunicorn_start
-echo '  --bind=$BIND \' >> /home/django/.venv/bin/gunicorn_start
-echo '  --log-level=$LOG_LEVEL \' >> /home/django/.venv/bin/gunicorn_start
-echo '  --log-file=-' >> /home/django/.venv/bin/gunicorn_start
+sudo -u django bash -c 'cat > /home/django/.venv/bin/gunicorn_start' <<EOF
+#!/bin/bash
+
+NAME="django_app"
+DIR=/home/django/$project
+USER=django
+GROUP=django
+WORKERS=3
+BIND=unix:/home/django/gunicorn.sock
+DJANGO_SETTINGS_MODULE=$djapp.settings
+DJANGO_WSGI_MODULE=$djapp.wsgi
+LOG_LEVEL=error
+
+source /home/django/.venv/bin/activate
+
+export DJANGO_SETTINGS_MODULE=\$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=\$DIR:\$PYTHONPATH
+
+exec /home/django/.venv/bin/gunicorn \${DJANGO_WSGI_MODULE}:application \
+  --name \$NAME \
+  --workers \$WORKERS \
+  --user=\$USER \
+  --group=\$GROUP \
+  --bind=\$BIND \
+  --log-level=\$LOG_LEVEL \
+  --log-file=-
+EOF
+
+sudo chmod u+x /home/django/.venv/bin/gunicorn_start
 
 echo "==15== Convertimos a Ejecutable el Fichero: gunicorn_start === "
 chmod u+x /home/django/.venv/bin/gunicorn_start
 
 echo "==16== Configurando Supervisor === "
-mkdir /home/django/logs
-touch /home/django/logs/gunicorn-error.log
-touch /etc/supervisor/conf.d/django_app.conf
-echo '[program:django_app]' >> /etc/supervisor/conf.d/django_app.conf
-echo 'command=/home/django/.venv/bin/gunicorn_start' >> /etc/supervisor/conf.d/django_app.conf
-echo 'user=django' >> /etc/supervisor/conf.d/django_app.conf
-echo 'autostart=true' >> /etc/supervisor/conf.d/django_app.conf
-echo 'autorestart=true' >> /etc/supervisor/conf.d/django_app.conf
-echo 'redirect_stderr=true' >> /etc/supervisor/conf.d/django_app.conf
-echo 'stdout_logfile=/home/django/logs/gunicorn-error.log' >> /etc/supervisor/conf.d/django_app.conf
+sudo -u django mkdir -p /home/django/logs
+sudo -u django touch /home/django/logs/gunicorn-error.log
+
+sudo tee /etc/supervisor/conf.d/django_app.conf > /dev/null <<EOF
+[program:django_app]
+command=/home/django/.venv/bin/gunicorn_start
+user=django
+autostart=true
+autorestart=true
+redirect_stderr=true
+stdout_logfile=/home/django/logs/gunicorn-error.log
+EOF
+
 sudo supervisorctl reread
 sudo supervisorctl update
 
@@ -146,9 +151,8 @@ sudo rm -f /etc/nginx/sites-enabled/default
 sudo service nginx restart
 
 echo "=== Finalizando ==="
-python /home/django/$project/manage.py migrate
-python /home/django/$project/manage.py collectstatic
-sudo chown django:django /home/django/* -R
-sudo chown django:django /home/django/.venv/* -R
-sudo chown django:django /home/django/.venv -R
+sudo -u django /home/django/.venv/bin/python /home/django/$project/manage.py migrate
+sudo -u django /home/django/.venv/bin/python /home/django/$project/manage.py collectstatic --noinput
+sudo chown django:django /home/django/ -R
+sudo chown django:django /home/django/.venv/ -R
 sudo supervisorctl restart django_app
